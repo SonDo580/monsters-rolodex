cd29e34f8dc7775b2ffad37db1a2f9993c791082
Son Do <dominhson580@gmail.com>
2023-05-06 16:43:00 +0700

    Add and style app title

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox onSearch={searchMonsters} placeholder="Search monsters" />
         <CardList monsters={filteredMonsters} />
       </div>
