ec5f76a4a8567ee27d69d12d27bb787ccf159ffe
Son Do <dominhson580@gmail.com>
2023-05-06 10:35:23 +0700

    Add state and render monster names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
-function App() {
-  return <>App</>;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        { name: "Linda", id: uuidv4() },
+        { name: "Frank", id: uuidv4() },
+        { name: "Jacky", id: uuidv4() },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
