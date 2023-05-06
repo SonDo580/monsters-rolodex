f9a427065a6ae3752420a778e9f1d0c1c9568ecf
Son Do <dominhson580@gmail.com>
2023-05-06 16:32:07 +0700

    Add Card details

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,7 +8,12 @@ export default class Card extends Component {
 
     return (
       <div className="card-container">
-        <h1>{monster.name}</h1>
+        <img
+          src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+          alt={monster.name}
+        />
+        <h2>{monster.name}</h2>
+        <p>{monster.email}</p>
       </div>
     );
   }
